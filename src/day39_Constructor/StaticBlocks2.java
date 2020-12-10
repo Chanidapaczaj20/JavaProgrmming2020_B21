2c62a689b0a666df92df1f6e066e8a8078e5e429
Chanidapaczaj20 <70244412+Chanidapaczaj20@users.noreply.github.com>
2020-12-10 11:53:15 -0500

    added day39

---
+++ src/day39_Constructor/StaticBlocks2.java
@@ -0,0 +1,30 @@
+package day39_Constructor;
+
+public class StaticBlocks2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Main Method");
+    }
+
+    static {
+        System.out.println("Static Block1");
+    }
+
+    static {
+        System.out.println("Static Block2");
+    }
+
+    static {
+        System.out.println("Static Block3");
+    }
+
+
+
+
+
+
+
+
+
+}
