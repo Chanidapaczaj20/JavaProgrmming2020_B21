62b302069551a491e4039a5ad9c977c37f110ee7
Chanidapaczaj20 <70244412+Chanidapaczaj20@users.noreply.github.com>
2020-12-18 15:20:27 -0500

    added day43

---
+++ src/day43_Inheritance/AccessModifiers.java
@@ -0,0 +1,25 @@
+package day43_Inheritance;
+
+public class AccessModifiers {
+
+   public static void publicMethod(){
+
+   }
+
+   protected static void protectedMethod(){
+
+   }
+
+   static void defaultMethod(){
+
+   }
+
+   private static void PrivateMethod(){
+
+   }
+
+
+
+
+
+}
